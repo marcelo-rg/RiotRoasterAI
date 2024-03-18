eda913fffc2436f8119643fbe142a8422ec1e102
marcelo-rg <mrgo2000@gmail.com>
2024-03-18 20:14:46 +0000

    delete useless files

--- opgg/__init__.py
+++
@@ -1,7 +0,0 @@
-from opgg.summoner import Summoner
-from opgg.season import Season, SeasonInfo
-from opgg.champion import ChampionStats, Champion, Spell, Passive, Skin, Price
-from opgg.league_stats import LeagueStats, Tier, QueueInfo
-from opgg.game import Game
-from opgg.params import Region, By
-from opgg.cacher import Cacher
